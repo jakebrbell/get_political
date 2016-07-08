0a18753cfaf70b81badf0fab5200f1be3d898d17
Jake Bell <jakebrbell@gmail.com>
2016-07-08 12:08:22 -0700

    Add migration for pols table

---
+++ migrations/20160708113620_pols.js
@@ -0,0 +1,24 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('pols', (table) => {
+    table.increments();
+    table.string('name').notNullable().defaultTo('');
+    table.string('title').notNullable().defaultTo('');
+    table.string('party').notNullable().defaultTo('');
+    table.string('street').notNullable().defaultTo('');
+    table.string('city').notNullable().defaultTo('');
+    table.specificType('state', 'char(2)').notNullable().defaultTo('');
+    table.string('zipcode').notNullable().defaultTo('');
+    table.string('phone').notNullable().defaultTo('');
+    table.string('twitter').notNullable().defaultTo('');
+    table.string('facebook').notNullable().defaultTo('');
+    table.string('youtube').notNullable().defaultTo('');
+    table.string('picture_url').notNullable().defaultTo('');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('pols');
+};
