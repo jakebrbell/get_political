8eb45ba1c495e6094b9a18918631d4a7b20b985f
Jake Bell <jakebrbell@gmail.com>
2016-07-08 12:04:05 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+'use strict';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ silent: true });
+}
+
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8000;
+
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+app.disable('x-powered-by');
+
+if (process.env.NODE_ENV !== 'test') {
+  const morgan = require('morgan');
+  app.use(morgan('short'));
+}
+
+app.use(bodyParser.json());
+
+request.get(`https://www.googleapis.com/civicinfo/v2/representatives?address=98109&key=${process.env.GOOGLE_CIVIC_API}`, (err, res, body) => {
+  if (err) {
+    return next(err);
+  }
+
+  console.log(JSON.parse(body).officials[0].photoUrl);
+});
+
+app.use((req, res, next) => {
+
+});
+
+app.use((_req, res) => {
+  res.sendStatus(404);
+});
+
+// eslint-disable-next-line max-params
+app.use((err, _req, res, _next) => {
+  // eslint-disable-next-line no-console
+  console.error(err.stack);
+  res.sendStatus(500);
+});
+
+app.listen(port, () => {
+  if (process.env.NODE_ENV !== 'test') {
+    // eslint-disable-next-line no-console
+    console.log('Listening on port', port);
+  }
+});
