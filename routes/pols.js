e786e0ed008a261d320fea08119af8a3d41bc6bc
Jake Bell <jakebrbell@gmail.com>
2016-07-12 11:20:08 -0700

    Close #6 Create pols zipcode search route

--- routes/pols.js
+++ routes/pols.js
@@ -1,18 +1,58 @@
 'use strict';
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ silent: true });
+}
+
 const express = require('express');
 const router = express.Router();
 const knex = require('../knex');
 
-router.get('/pols', (_req, res, next) => {
-  knex('pols')
-    .orderBy('id')
-    .then((pols) => {
-      res.send(pols);
-    })
-    .catch((err) => {
-      next(err);
-    });
+const request = require('request-promise');
+
+router.get('/pols', (req, res, next) => {
+  if (Object.keys(req.query).length !== 0) {
+    const zip = Number.parseInt(req.query.zip);
+
+    if (Number.isNaN(zip)) {
+      return next();
+    }
+
+    request.get(`https://congress.api.sunlightfoundation.com/legislators/locate?zip=${zip}&apikey=${process.env.SUNLIGHT_API}`)
+      .then((response) => {
+        const parsedResults = JSON.parse(response).results;
+
+        const bioguide_ids = [];
+
+        for (const pol of parsedResults) {
+          bioguide_ids.push(pol.bioguide_id);
+        }
+
+        return(bioguide_ids);
+      })
+      .then((bioguide_ids) => {
+        knex('pols')
+          .where('bioguide_id', bioguide_ids[0])
+          .orWhere('bioguide_id', bioguide_ids[1])
+          .orWhere('bioguide_id', bioguide_ids[2])
+          .then((pols) => {
+            res.send(pols);
+          })
+          .catch((err) => {
+            next(err);
+          });
+      })
+  }
+  else {
+    knex('pols')
+      .orderBy('id')
+      .then((pols) => {
+        res.send(pols);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
 });
 
 router.get('/pols/:id', (req, res, next) => {
